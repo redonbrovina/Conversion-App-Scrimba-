906b9a619bdb7a3afb207caf42f3170b9b3e0229
Redon <redonbrovina@gmail.com>
2024-07-18 17:22:33 +0200

    commit

---
+++ index.js
@@ -0,0 +1,34 @@
+let inputEl = document.getElementById("input-el");
+const btnEl = document.getElementById("btn-el");
+let lengthEl = document.getElementById("length-el");
+let volumeEl = document.getElementById("volume-el");
+let massEl = document.getElementById("mass-el");
+
+function length_func() {
+    let mf = inputEl.value * 3.281;
+    mf = mf.toFixed(3);
+    let fm = inputEl.value / 3.281;
+    fm = fm.toFixed(3);
+    lengthEl.textContent = `${inputEl.value} meters = ${mf} feet | ${inputEl.value} feet = ${fm} meters`;
+}
+function volume_func() {
+   let lg = inputEl.value / 3.785;
+   lg = lg.toFixed(3);
+   let gl = inputEl.value * 3.785;
+   gl = gl.toFixed(3);
+   volumeEl.textContent = `${inputEl.value} liters = ${lg} gallons | ${inputEl.value} gallons = ${gl} liters`;
+   
+}
+function mass_func() {
+    let kp = inputEl.value * 2.205;
+    kp = kp.toFixed(3);
+    let pk = inputEl.value / 2.205;
+    pk = pk.toFixed(3);
+    massEl.textContent = `${inputEl.value} kilograms = ${kp} pounds | ${inputEl.value} pounds = ${pk} kilograms`;
+}
+
+btnEl.addEventListener("dblclick", function() {
+    length_func();
+    volume_func();
+    mass_func();
+})
