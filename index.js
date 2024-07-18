ac90a20efed56f349e1f0bf87374f9749d9d7fbb
Redon <redonbrovina@gmail.com>
2024-07-18 18:03:40 +0200

    git-commit

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const btnEl = document.getElementById("btn-el");
 let lengthEl = document.getElementById("length-el");
 let volumeEl = document.getElementById("volume-el");
 let massEl = document.getElementById("mass-el");
+let btnS = document.getElementById("btn-s");
+let bottom = document.getElementById("bottom");
+let btnT = document.getElementById("btn-t");
 
 function length_func() {
     let mf = inputEl.value * 3.281;
@@ -31,4 +34,32 @@ btnEl.addEventListener("dblclick", function() {
     length_func();
     volume_func();
     mass_func();
+});
+
+btnS.addEventListener("click", function() {
+    bottom.style.backgroundColor =  '#1F2937';
+    bottom.style.transitionDuration = '.5s';
+    let box = document.querySelectorAll(".box");
+    let res = document.querySelectorAll(".res");
+    for(let i = 0; i<box.length; i++){
+        box[i].style.backgroundColor = '#273549';
+        box[i].style.color = '#CCC1FF';
+        res[i].style.color = 'white';
+        box[i].style.transitionDuration = '.5s';
+    }
+
+})
+
+btnT.addEventListener("click", function() {
+    bottom.style.backgroundColor = '#F4F4F4';
+    bottom.style.transitionDuration = '.5s';
+    let box = document.querySelectorAll(".box");
+    let res = document.querySelectorAll(".res");
+    for(let i = 0; i<box.length; i++){
+        box[i].style.backgroundColor = 'white';
+        box[i].style.color = '#5A537B';
+        res[i].style.color = '#353535';
+        box[i].style.transitionDuration = '.5s';
+        res[i].style.transitionDuration = '.5s';
+    }
 })
